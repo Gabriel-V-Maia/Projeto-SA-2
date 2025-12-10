d12530bd719d326d68a16741b882fc29eeeb71df
Gabriel-V-Maia <gabrielvinicius.damaia@gmail.com>
2025-12-09 21:46:53 -0300

    fix: enviar dados da sessão em páginas de detalhes

--- src/routes/produtos.py
+++ src/routes/produtos.py
@@ -1,7 +1,6 @@
 import sqlite3
-from flask import render_template, jsonify
-from app import app
-from app import get_db
+from flask import render_template, session
+from app import app, get_db
 
 DATABASE = 'automax.db'
 
@@ -9,19 +8,16 @@ DATABASE = 'automax.db'
 def get_product(id):
     db = get_db()
     cursor = db.cursor()
-
     cursor.execute('''
         SELECT id_produto, nome, preco, stock, detalhes, categoria, imagem
         FROM produtos
         WHERE id_produto = ?
     ''', (id,))  
-
     produto = cursor.fetchone()
     
-
     if produto is None:
         return render_template('notfound.html')
-
+    
     produto_dict = {
         "id": produto[0],
         "nome": produto[1],
@@ -31,5 +27,11 @@ def get_product(id):
         "categoria": produto[5],
         "imagem": produto[6]
     }
-
-    return render_template('/produto/detalhe.html', produto=produto_dict)
+    
+    usuario = session.get("usuario")
+    tipo = session.get("tipo")
+    
+    return render_template('produto/detalhe.html', 
+                         produto=produto_dict,
+                         usuario_logado=usuario,
+                         tipo_usuario=tipo)
